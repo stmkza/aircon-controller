650576e2685fe6afa066df4fddaeea53eee91dca
stmkza <stmkza@gmail.com>
2019-08-27 20:03:57 +0900

    表示の仕組みを改善

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,20 @@ const controller = new AirconController((process.env.AIRCON_IP || ''), 3610);
 
 async function main() {
     await controller.initialize();
-    console.log((await controller.getOperationState() === OperationState.ON) ? 'ON' : 'OFF');
-    console.log((await controller.getPowerSavingOperationSetting() === PowerSavingOperationSetting.ON) ? 'ON' : 'OFF');
+    await displayOnOffInformation(controller.getOperationState.bind(controller), 'Operation status', OperationState.ON, OperationState.OFF);
+    await displayOnOffInformation(controller.getPowerSavingOperationSetting.bind(controller), 'Power-saving operation status', PowerSavingOperationSetting.ON, PowerSavingOperationSetting.OFF);
     await controller.finalize();
 }
 
 main();
+
+async function displayOnOffInformation(func: Function, label: string, onValue: any, offValue: any) {
+    const result = await func();
+    if(result === onValue) {
+        console.log(`${label}: ON`);
+    } else if(result === offValue) {
+        console.log(`${label}: OFF`);
+    } else {
+        console.log(`${label}: Invalid value (hex ${result.toString(16).padStart(2, '0')})`);
+    }
+}
